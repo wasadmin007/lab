22a4e4728cade82b61141822c3c0ca919782463b
praveen jeendru <praveenjeendru@praveens-MacBook-Pro-2.local>
2026-02-02 19:40:14 -0600

    outputs files

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "applicant_name" {
+  description = "Name of the applicant"
+  value       = "Praveen Jeendru"
+}
+
+output "position_title" {
+  description = "Title of the position applied for"
+  value       = "DevOps Engineer"
+}
